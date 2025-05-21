3bb5682b2fe4d28493bd21b0b37fe048ee328cbf
Sunapp <58972330+Sunappu-Awoken@users.noreply.github.com>
2025-05-21 17:15:55 +0000

    Add files via upload
    the hidden files (.)  have been removed from the name to be added into here

---
+++ inc/nav.inc.php
@@ -0,0 +1,15 @@
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark" style="background:#2b0155!important;">
+	<div class="container-fluid">
+		<ul class="navbar-nav">
+			<li class="nav-item">
+				<a href="index.php" class="nav-link" style="font-size:1.5rem;">PC URL</a>
+			</li>
+			<!--<li class="nav-item">
+				<a href="mktg.php" class="nav-link">Mktg-XLS</a>
+			</li>-->
+			<li class="nav-item">
+				<div id="unTopDiv"></div>
+			</li>
+		</ul>
+	</div>
+</nav>
