a0a0bf850487c13c47742359035c7c9ff2c07fd8
Sunapp <58972330+Sunappu-Awoken@users.noreply.github.com>
2025-05-21 17:07:01 +0000

    Delete url.peacey.us directory
    Replace This with the True Origin Backup

--- url.peacey.us/inc/temp_landing_placeholder.inc.php
+++
@@ -1,7 +0,0 @@
-<style>
-	html,body{min-height:97vh;}
-	body{font-family:"Segoe UI";background:#330033;}
-	section{display:flex;justify-content:center;align-items:center;min-height:97vh;}
-	div{font-size:5rem;color:darkslateblue;}
-</style>
-<section><div>url.peacey.us</div></section>
